f459d73606b6f2e37a656b9bfb1a0a5893046533
Cassandre Tirions <ctirions@shi-r11-p4.s19.be>
2022-10-03 19:35:54 +0200

    Begin of MariaDb

---
+++ srcs/mariadb/utils/entrypoint.sh
@@ -0,0 +1,31 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    entrypoint.sh                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/10/03 18:19:13 by ctirions          #+#    #+#              #
+#    Updated: 2022/10/03 19:33:31 by ctirions         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/sh
+
+if [ ! -d /var/lib/mysql/$MARIADB_NAME ]; then
+	service mysql start
+
+	mysql -e "UPDATE mysql.user SET Password = PASSWORD('$MARIADB_PSW') WHERE User = 'root'"
+	mysql -e "DELETE FROM mysql.user WHERE User='';"
+	mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+	mysql -e "DROP DATABASE test;"
+
+	echo "Create $MARIADB_NAME"
+	mysql -e "CREATE DATABASE $MARIADB_NAME"
+	mysql -e "CREATE USER '$MARIADB_USER_NAME'@'localhost' IDENTIFIED BY '$MARIADB_USER_PSW'"
+	mysql -e "GRANT ALL PRIVILEGES ON $MARIADB_NAME.* TO $MARIADB_USER_NAME@'localhost'"
+
+	mysql -e "FLUSH PRIVILEGES"
+
+	service mysql stop
+fi
