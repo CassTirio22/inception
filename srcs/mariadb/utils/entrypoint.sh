cee5b96c9fd0754213562e1248c622fdef42e41f
Cassandre Tirions <ctirions@fu-r6-p6.s19.be>
2022-10-17 15:37:54 +0200

    Work in progress

--- srcs/mariadb/utils/entrypoint.sh
+++ srcs/mariadb/utils/entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/03 18:19:13 by ctirions          #+#    #+#              #
-#    Updated: 2022/10/17 15:19:19 by ctirions         ###   ########.fr        #
+#    Updated: 2022/10/17 15:37:51 by ctirions         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,9 +18,15 @@ if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
 	echo "Create $MARIADB_DATABSE"
 	eval "echo \"$(cat ./config.sql)\"" | mariadb -u root
 
+	echo "TEST"
+
 	mysqladmin -u root password $MARIADB_PSW
 
+	echo "TEST2"
+
 	service mysql stop --datadir=/var/lib/mysql
+
+	echo "TEST3"
 
 fi
 
